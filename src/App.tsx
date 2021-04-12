24c78139739118ec34b1b7a7270e670932dfb6e1
victor-osf <victor.rocha@ifactory.com.br>
2021-04-12 15:53:28 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="main">
+        <div className="container">
+          <div className="search">
+            <FontAwesomeIcon icon={faSearch} size="lg" color="#b0a4df" />
+            <input type="text" placeholder="Find your gif"/> 
+          </div>
+          <ul className="grid">
+            <li className="card">
+              <img src="https://images.pexels.com/photos/1626481/pexels-photo-1626481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+            </li>
+            <li className="card">
+              <img src="https://images.pexels.com/photos/1626481/pexels-photo-1626481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+            </li>
+            <li className="card">
+              <img src="https://images.pexels.com/photos/1626481/pexels-photo-1626481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+            </li>
+            <li className="card">
+              <img src="https://images.pexels.com/photos/1626481/pexels-photo-1626481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+            </li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
