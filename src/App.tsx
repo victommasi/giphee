f9ca20024cdb6484d68e4e933b0d18e108d70499
victor-osf <victor.rocha@ifactory.com.br>
2021-04-15 14:56:30 -0300

    tranfering logic do parent (App)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,30 +10,100 @@ import './App.css';
 
 import Grid from '././components/Grid';
 
-function App() {
+const App = () => {
+  const scrollObserver = React.useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  const [gifs, setGifs] = useState<any>([]);
   const [term, setTerm] = useState('');
-  const [gifs, setGifs] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [scrollRatio, setScrollRatio] = useState<any | null>(null);
+
+  useEffect(() => {
+    const loadGifs = async () => {
+      const response = await fetchTredingGifs(0);
+      setGifs(response.data.data);
+    }
+
+    loadGifs();
+  }, [])
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      const ratio = entries[0].intersectionRatio;
+      setScrollRatio(ratio);
+    })
+
+    intersectionObserver.observe(scrollObserver.current)
+
+    return () => {
+      intersectionObserver.disconnect();
+    }
+  }, [])
+
+  useEffect(() => {
+    const loadMoreGifs = async (term: string) => {
+      const newOffset = offset + 10;
+      let response;
+      setOffset(newOffset)
+
+      if (term === '') {
+        response = await fetchTredingGifs(newOffset);
+      } else {
+        response = await searchGifs(term, newOffset);
+      }
+
+      const newGifs = [...gifs]
+      newGifs.push(...response.data.data)
+      setGifs(newGifs);
+    }
+
+
+    if (scrollRatio > 0 && gifs.length > 0) {
+      loadMoreGifs(term);
+    }
+
+  }, [scrollRatio])
 
   const handleSearch = async (e: any) => {
     let newTerm = e.target.value;
+    let response
 
     if (newTerm.length > 1) {
-      const response = await api.get('/search', {
-        params:
-        {
-          api_key: 'Ykd0y68FN92mO1Lc6jR8iCnYhtEKL6Ze',
-          q: newTerm,
-          limit: 12,
-          offset: 0,
-          rating: 'g'
-        }
-      });
-      setTerm(newTerm);
-      
+      response = await searchGifs(newTerm, 0);
       const searchedGifs = response.data.data
       searchedGifs.length > 0 ? setGifs(searchedGifs) : setGifs([]);
+    } else {
+      response = await fetchTredingGifs(0);
+      setGifs(response.data.data);
     }
-  } 
+
+    setTerm(newTerm);
+  }
+
+  const searchGifs = async (term: string, offset: number) => {
+    return await api.get('/search', {
+      params:
+      {
+        api_key: 'Ykd0y68FN92mO1Lc6jR8iCnYhtEKL6Ze',
+        q: term,
+        limit: 12,
+        offset: offset,
+        rating: 'g'
+      }
+    });
+  }
+
+  const fetchTredingGifs = async (offset: number) => {
+    return await api.get('/trending', {
+      params:
+      {
+        api_key: 'Ykd0y68FN92mO1Lc6jR8iCnYhtEKL6Ze',
+        limit: 12,
+        offset: offset,
+        rating: 'g'
+      }
+    });
+  }
 
   return (
     <div className="App">
@@ -40,9 +111,10 @@ function App() {
         <div className="container">
           <div className="search">
             <FontAwesomeIcon icon={faSearch} size="lg" color="#b0a4df" />
-            <input type="text" placeholder="Find your gif" onChange={handleSearch} /> 
+            <input type="text" placeholder="Find your gif" onChange={handleSearch} />
           </div>
-          <Grid gifs={gifs}/>
+          <Grid gifs={gifs} />
+          <div ref={scrollObserver}></div>
         </div>
       </main>
     </div>
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,77 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, useRef } from 'react';
-
-import api from '../../services/api'
-
-const Grid = (parentGifs) => {
-  const scrollObserver = useRef();
-
-  const [gifs, setGifs] = useState([]);
-  const [offset, setOffset] = useState(0);
-  const [scrollRatio, setScrollRatio] = useState(null);
-
-  useEffect(() => {
-    const loadGifs = async () => {
-      const response = await api.get('/trending', {
-        params:
-        {
-          api_key: 'Ykd0y68FN92mO1Lc6jR8iCnYhtEKL6Ze',
-          limit: 12,
-          offset: 0,
-          rating: 'g'
-        }
-      });
-      setGifs(response.data.data);
-    }
-
-    loadGifs();
-  }, [])
-
-  useEffect(() => {
-    const intersectionObserver = new IntersectionObserver((entries) => {
-      const ratio = entries[0].intersectionRatio;
-      setScrollRatio(ratio);
-    })
-
-    intersectionObserver.observe(scrollObserver.current)
-
-    return () => {
-      intersectionObserver.disconnect();
-    }
-  }, [])
-
-  useEffect(() => {
-    const loadMoreGifs = async () => {
-      const newOffset = offset + 10;
-      setOffset(newOffset)
-
-      const response = await api.get('/trending', {
-        params:
-        {
-          api_key: 'Ykd0y68FN92mO1Lc6jR8iCnYhtEKL6Ze',
-          limit: 12,
-          offset: newOffset,
-          rating: 'g'
-        }
-      });
-
-      const newGifs = [...gifs]
-      newGifs.push(...response.data.data)
-      setGifs(newGifs);
-    }
-
-
-    if (scrollRatio > 0) {
-      loadMoreGifs();
-    }
-  }, [scrollRatio])
-
-  useEffect(() => {
-    setGifs(parentGifs.gifs);
-
-    return [];
-  }, [parentGifs])
-
+const Grid = ({gifs}) => {
   return (
     <ul className="grid">
       {gifs.map((gif, index) => (
@@ -79,7 +6,6 @@ const Grid = (parentGifs) => {
           <img alt={gif.title} title={gif.title} src={gif.images.preview_gif.url}></img>
         </li>
       ))}
-      <div ref={scrollObserver}></div>
     </ul>
   );
 }
