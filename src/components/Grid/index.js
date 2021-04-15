61155afa98d5d1c5f496ca39e9e93e8ad70025b2
victor-osf <victor.rocha@ifactory.com.br>
2021-04-15 15:44:38 -0300

    applying hover

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -4,6 +4,7 @@ const Grid = ({gifs}) => {
       {gifs.map((gif, index) => (
         <li className="card" key={index}>
           <img alt={gif.title} title={gif.title} src={gif.images.preview_gif.url}></img>
+          <p className="title">{gif.title}</p>
         </li>
       ))}
     </ul>
